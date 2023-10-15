180a3100ba7696a53465cc5fcb8dc8b8dd2a41c1
Mansoor Khan <thisismansoorkhan@gmail.com>
2023-10-15 17:32:34 +0530

    Create, show/hide add photo form, display photos: useMemo, useEffect, useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import {useEffect, useState} from "react";
+import UploadForm from "./components/UploadForm";
+
+const photos = [
+    'https://picsum.photos/id/1001/200/200',
+    'https://picsum.photos/id/1002/200/200',
+    'https://picsum.photos/id/1003/200/200',
+    'https://picsum.photos/id/1004/200/200',
+    'https://picsum.photos/id/1005/200/200',
+    'https://picsum.photos/id/1006/200/200',
+    'https://picsum.photos/id/1008/200/200',
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [count, setCount] = useState(0);
+    const [inputs, setInputs] = useState({ title: null, file: null, path: null });
+    const [items, setItems] = useState(photos);
+    const [isCollapsed, collapse] = useState(false);
+    const toggle = () => collapse(!isCollapsed);
+    const handleOnChange = (e) => {
+        if (e.target.name === 'file') {
+            setInputs({ ...inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])})
+        } else {
+            setInputs({...inputs, title: e.target.value })
+        }
+    }
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        setItems([inputs.path, ...items])
+        setInputs({title: '', file: null, path: null })
+        collapse(false)
+    }
+
+    useEffect(() => {
+        setCount(`You have ${items.length} image${items.length > 1 ? 's': ''}`);
+    },[items]);
+
+    return (
+      <>
+        <Navbar />
+        <div className="container text-center mt-5">
+            <button className="btn btn-success float-end" onClick={toggle}>{isCollapsed ? 'Close' : '+ Add'}</button>
+            <div className="clearfix mb-4"></div>
+            <UploadForm
+                inputs={inputs}
+                isVisible={isCollapsed}
+                onChange={handleOnChange}
+                onSubmit={handleOnSubmit}
+            />
+            {count}
+            <h1>Gallery</h1>
+            <div className="row">
+              {items.map((photo, index) => <Card key={index} src={photo} />)}
+            </div>
+        </div>
+      </>
+    );
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+const Card = (props) => {
+    return (
+        <div className="col mb-5">
+            <div className="card" style={{width: "18rem"}}>
+                <img src={props.src} className="card-img-top" alt="placeholder image" />
+            </div>
+        </div>
+    )
+}
+export default Card;
