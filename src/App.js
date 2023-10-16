3d8df153645ab52b9e017224935122c042a3ba2e
Mansoor Khan <thisismansoorkhan@gmail.com>
2023-10-16 14:11:55 +0530

    Replace useState() with useReducer to handle form operations

--- src/App.js
+++ src/App.js
@@ -1,60 +1,85 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
-import {useEffect, useState} from "react";
+import {useEffect, useReducer, useState} from "react";
 import UploadForm from "./components/UploadForm";
 
 const photos = [
-    'https://picsum.photos/id/1001/200/200',
-    'https://picsum.photos/id/1002/200/200',
-    'https://picsum.photos/id/1003/200/200',
-    'https://picsum.photos/id/1004/200/200',
-    'https://picsum.photos/id/1005/200/200',
-    'https://picsum.photos/id/1006/200/200',
-    'https://picsum.photos/id/1008/200/200',
 ];
+
+const handleOnChange = (state, e) => {
+    if (e.target.name === 'file') {
+        return ({ ...state.inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])})
+    } else {
+        return ({ ...state.inputs, title: e.target.value })
+    }
+}
+
+function itemsReducer(state ,action) {
+    switch(action.type) {
+        case 'setItem': {
+            return {
+                ...state,
+                items: [state.inputs, ...state.items]
+            }
+        }
+        case 'setInputs': {
+            return {
+                ...state,
+                inputs: handleOnChange(state, action.payload.value)
+            }
+        }
+        case 'collapse': {
+            return {
+                ...state,
+                isCollapsed: action.payload.bool
+            }
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
+const initialState = {
+    items: photos,
+    count: photos.length,
+    inputs: { title: null, file: null, path: null},
+    isCollapsed: false
+}
 
 function App() {
 
+    const [state, dispatch] = useReducer(itemsReducer, initialState);
     const [count, setCount] = useState(0);
-    const [inputs, setInputs] = useState({ title: null, file: null, path: null });
-    const [items, setItems] = useState(photos);
-    const [isCollapsed, collapse] = useState(false);
-    const toggle = () => collapse(!isCollapsed);
-    const handleOnChange = (e) => {
-        if (e.target.name === 'file') {
-            setInputs({ ...inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])})
-        } else {
-            setInputs({...inputs, title: e.target.value })
-        }
-    }
+    const toggle = (bool) => dispatch({ type: 'collapse', payload: { bool: bool }})
+    const handleOnChange = (e) => dispatch({ type: 'setInputs', payload: { value: e }})
     const handleOnSubmit = (e) => {
         e.preventDefault();
-        setItems([inputs.path, ...items])
-        setInputs({title: '', file: null, path: null })
-        collapse(false)
+        dispatch({ type: 'setItem' });
+        toggle(!state.isCollapsed);
     }
 
     useEffect(() => {
-        setCount(`You have ${items.length} image${items.length > 1 ? 's': ''}`);
-    },[items]);
+        setCount(`You have ${state.items.length} image${state.items.length > 1 ? 's': ''}`);
+    },[state.items]);
 
     return (
       <>
         <Navbar />
         <div className="container text-center mt-5">
-            <button className="btn btn-success float-end" onClick={toggle}>{isCollapsed ? 'Close' : '+ Add'}</button>
+            <button className="btn btn-success float-end" onClick={() => toggle(!state.isCollapsed)}>{state.isCollapsed ? 'Close' : '+ Add'}</button>
             <div className="clearfix mb-4"></div>
             <UploadForm
-                inputs={inputs}
-                isVisible={isCollapsed}
+                inputs={state.inputs}
+                isVisible={state.isCollapsed}
                 onChange={handleOnChange}
                 onSubmit={handleOnSubmit}
             />
             {count}
             <h1>Gallery</h1>
             <div className="row">
-              {items.map((photo, index) => <Card key={index} src={photo} />)}
+              {state.items.map((photo, index) => <Card key={index} src={photo.path} />)}
             </div>
         </div>
       </>
